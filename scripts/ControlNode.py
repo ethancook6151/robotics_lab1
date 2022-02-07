b15107055cff7b2d20c75beb0fa4de4a7f4cd406
ethancook6151 <etc6151uncw@gmail.com>
2022-02-07 16:21:37 +0000

    final changes

--- scripts/ControlNode.py
+++ scripts/ControlNode.py
@@ -17,7 +17,7 @@ def pose_callback(data):
 	global pos_msg
 	pos_msg.x = data.x
 	
-def conrtol_callback(data):
+def control_callback(data):
 	global turtle_msg
 	turtle_msg.xd = data.xd
 	turtle_msg.kp = data.kp
@@ -26,13 +26,13 @@ def conrtol_callback(data):
 if __name__ == '__main__':
 	rospy.init_node('ControlNode', anonymous=True)
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
-	rospy.Subscriber('/turtle1/control' Turtlecontrol, control_callback)
+	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, control_callback)
 	pos_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 	loop_rate = rospy.Rate(10)
 
 	# run a control loop on a regular basis (every 0.1 second)
 	while not rospy.is_shutdown():
-		twist.linear.x = control_msg.kp * (control_msg.xd - pos_msg.x)
-		cmd_pub.publish(twistros) 
+		twist.linear.x = turtle_msg.kp * (turtle_msg.xd - pos_msg.x)
+		pos_pub.publish(twist) 
 		# wait for 0.1 of a second and go to next step 
 		loop_rate.sleep()
